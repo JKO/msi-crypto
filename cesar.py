46d3265e7e8ffcca0d4d2213cc4b77f4dc8f48aa
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2019-07-26 16:44:39 -0500

    Se adiciona el manejo ascii art para el banner XD

--- cesar.py
+++ cesar.py
@@ -14,12 +14,17 @@
 #Importarmos las librerias necesarias, para uso de argumentos y la generacion del hash md5
 import argparse
 import hashlib
+#Banner al Inicio
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("Cifrado Cesar")
+print(ascii_banner)
+print("Por Jaime Restrepo y Jacobo Tibaquirá\n\n")
 
 #Parseamos los parametros de entrada
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', help='mensaje a cifrar/descifrar', dest='mensaje')
 parser.add_argument('-f', help='Archivo a cifrar/descifrar', dest='archivo')
-parser.add_argument('-o', help='Archivo de Salida cidrado/descifrado', dest='salida')
+parser.add_argument('-o', help='Archivo de Salida cifrado/descifrado', dest='salida')
 parser.add_argument('-modo', help='modo a utilizar cifrar/descifrar', dest='modo')
 parser.add_argument('-ll', help='llave de cifrar/descifrado', dest='llave', type=int)
 args = parser.parse_args()
@@ -27,10 +32,8 @@ args = parser.parse_args()
 #Definimos las variables con base en los parametros ingresados
 ARCHIVO = args.archivo
 SALIDA = args.salida
-MENSAJE =  args.mensaje
 LLAVE = args.llave
 MODO = args.modo
-
 CARACTERES = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz'
 TRADUCIDO = ''
 
